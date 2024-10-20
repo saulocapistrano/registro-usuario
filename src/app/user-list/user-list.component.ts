05197f7dd0b460a6a85cd7b47f2cfa67d0b58cff
saulocapistrano <saulocapistrano@gmail.com>
2024-10-20 10:35:21 -0300

    feat: craido compontente de listar usuario

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService, User } from '../services/user.service';
 
 @Component({
   selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  templateUrl: './user-list.component.html'
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  users: User[] = [];
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
 }
